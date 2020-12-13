68790f25e0f0f93bca2965e0bc8250b019741894
onc157 <onc157@yandex.ru>
2020-12-13 22:18:55 +0300

    tasks fixed

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -87,8 +87,8 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(a, b, c) {
-  return (a + b > c) && (a + c > b) && (b + c > a);
+function isTriangle(/* a, b, c */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -124,8 +124,8 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(rect1, rect2) {
-  return (rect1.left + rect1.width >= rect2.left) && (rect1.top + rect1.height >= rect2.top);
+function doRectanglesOverlap(/* rect1, rect2 */) {
+  throw new Error('Not implemented');
 }
 
 
